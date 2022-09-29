64a933d9e6ffb47676931aee1c4667134f44311c
Olivier Huber <huber.olivier@gmail.com>
2022-09-29 16:20:54 +0200

    3. TTC, 1 art, CA

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,20 @@ use std::io;
 fn main() {
     println!("Welcome to the GoodSell Hardware-store!");
 
-    println!("Please input your email to receive our newsletter:");
+    println!("You are buying from our California store!");
 
-    let mut email = String::new();
+    println!("Please enter the price for your item?");
+
+    let mut price = String::new();
 
     io::stdin()
-        .read_line(&mut email)
+        .read_line(&mut price)
         .expect("Failed to read line");
 
-    println!("Thank you for subscribing to our newsletter {email}");
+    let price: f64 = price.trim().parse().expect("Please type a number!");
+
+    const CA_TAX_RATE: f64 = 8.25;
+    let total_price: f64 = price * (1.0 + CA_TAX_RATE/100.0);
+
+    println!("This will cost you ${total_price}");
 }
