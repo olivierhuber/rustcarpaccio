20e031e606cc67aecaaa3ecd164285a2a3eeb6d7
Olivier Huber <huber.olivier@gmail.com>
2022-09-29 16:38:14 +0200

    4. TTC, N art, CA

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
 
     println!("You are buying from our California store!");
 
-    println!("Please enter the price for your item?");
+    println!("Please enter the item price?");
 
     let mut price = String::new();
 
@@ -15,8 +15,18 @@ fn main() {
 
     let price: f64 = price.trim().parse().expect("Please type a number!");
 
-    const CA_TAX_RATE: f64 = 8.25;
-    let total_price: f64 = price * (1.0 + CA_TAX_RATE/100.0);
+    println!("Please enter the number of item(s)?");
 
-    println!("This will cost you ${total_price}");
+    let mut items = String::new();
+
+    io::stdin()
+        .read_line(&mut items)
+        .expect("Failed to read line");
+
+    let items: u32 = items.trim().parse().expect("Please type a number!");
+
+    const CA_TAX_RATE: f64 = 8.25;
+    let total_price: f64 = items as f64 * price * (1.0 + CA_TAX_RATE/100.0);
+
+    println!("This will cost you ${total_price:.2}");
 }
