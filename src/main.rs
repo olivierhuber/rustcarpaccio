16f3a0bdaa6028034a04b182887e56a3f3e55928
Olivier Huber <huber.olivier@gmail.com>
2022-09-29 16:08:45 +0200

    2. Input -> Output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the GoodSell Hardware-store!");
+
+    println!("Please input your email to receive our newsletter:");
+
+    let mut email = String::new();
+
+    io::stdin()
+        .read_line(&mut email)
+        .expect("Failed to read line");
+
+    println!("Thank you for subscribing to our newsletter {email}");
 }
